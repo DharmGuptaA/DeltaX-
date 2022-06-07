ac5384dd3c5750879da4c9b61538cd842ace7fe5
DharmGuptaA <68536319+DharmGuptaA@users.noreply.github.com>
2022-06-07 23:59:38 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+	<div class="container">
+		<footer class="border-top border-dark row mt-3 mt-md-5 pt-2">
+			<div class="col-md-3  text-center">
+				Artwork
+			</div>
+			<div class="col-md-3  text-center text-dark">
+				<a href="#" title="" class="text-dark">song</a>
+			</div>
+			<div class="col-md-3  text-center text-dark">
+				<a href="#" title="" class="text-dark">Date of Release </a>
+			</div>
+			<div class="col-md-3 text-center">
+				<a href="#" title="" class="text-dark">Artist</a>
+			</div>
+			<div class="col-md-3 text-center">
+				<a href="#" title="" class="text-dark">rate</a>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
